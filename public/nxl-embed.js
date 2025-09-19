3dcce8910725de446462551802cf311f750c4584
EThorntonNexusLuma <ethornton@nexusluma.com>
2025-09-19 14:23:57 -0400

    Update nxl-embed.js

--- public/nxl-embed.js
+++ public/nxl-embed.js
@@ -1,1 +1,7 @@
+<script src="https://ethorntonnexusluma.github.io/AIBusinesshelper/nxl-embed.js"
+  data-host="https://ethorntonnexusluma.github.io/AIBusinesshelper/"
+  data-width="420px"
+  data-height="640px"
+  data-open="false"
+  data-color="#8b5cf6"></script>
 
