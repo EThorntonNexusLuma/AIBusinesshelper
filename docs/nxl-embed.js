b43b6ecea84331c84f607ea83ae3b7e41d480866
EThorntonNexusLuma <ethornton@nexusluma.com>
2025-09-19 14:28:42 -0400

    Delete docs/nxl-embed.js

--- docs/nxl-embed.js
+++
@@ -1,7 +0,0 @@
-<script src="https://ethorntonnexusluma.github.io/AIBusinesshelper/nxl-embed.js"
-  data-host="https://ethorntonnexusluma.github.io/AIBusinesshelper/"
-  data-width="420px"
-  data-height="640px"
-  data-open="false"
-  data-color="#8b5cf6"></script>
-
