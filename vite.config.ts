8117fde68295c5df8a4a841c04d8d2d86ff0e0b0
EThorntonNexusLuma <ethornton@nexusluma.com>
2025-09-19 12:49:47 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // 👇 MUST match your repo name on GitHub
   base: '/AIBusinesshelper/',
   plugins: [react()],
 })
